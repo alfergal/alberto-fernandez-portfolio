1ed7662a62edb4469e77780f649be0f4f555d299
Alberto Fernández <albertofer1997@gmail.com>
2026-07-12 00:54:28 +0200

    Center elements in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen flex-col">
         <SiteHeader />
-        {children}
+        <main className="flex-1">{children}</main>
         <SiteFooter />
       </body>
     </html>
